ad174f6b7fa74817cc794f7c2ec04d20cdbeb28b
Eric Abell <ericabell@users.noreply.github.com>
2017-09-13 11:20:33 -0500

    Basic assignment done
    Just tracking three different items

--- src/Backpack.java
+++ src/Backpack.java
@@ -1,4 +1,8 @@
 public class Backpack {
+    private static int totalPencils = 0;
+    private static int totalPens = 0;
+    private static int totalNotebooks = 0;
+
     private int pencils;
     private int pens;
     private int notebooks;
@@ -7,5 +11,21 @@ public class Backpack {
         this.pencils = pencils;
         this.pens = pens;
         this.notebooks = notebooks;
+
+        totalPencils += pencils;
+        totalPens += pens;
+        totalNotebooks += notebooks;
+    }
+
+    public static int getTotalPencils() {
+        return totalPencils;
+    }
+
+    public static int getTotalPens() {
+        return totalPens;
+    }
+
+    public static int getTotalNotebooks() {
+        return totalNotebooks;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
 	// write your code here
         Backpack b1 = new Backpack(2,3,4);
         Backpack b2 = new Backpack(3,2,4);
+
+        System.out.println(Backpack.getTotalNotebooks());
+        System.out.println(Backpack.getTotalPencils());
+        System.out.println(Backpack.getTotalPens());
     }
 }
