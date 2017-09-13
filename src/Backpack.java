2e2380939ab97d1d0d1e324a5f9028407be36a5e
Eric Abell <ericabell@users.noreply.github.com>
2017-09-13 11:10:02 -0500

    Initial commit

---
+++ src/Backpack.java
@@ -0,0 +1,11 @@
+public class Backpack {
+    private int pencils;
+    private int pens;
+    private int notebooks;
+
+    public Backpack(int pencils, int pens, int notebooks) {
+        this.pencils = pencils;
+        this.pens = pens;
+        this.notebooks = notebooks;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Backpack b1 = new Backpack(2,3,4);
+        Backpack b2 = new Backpack(3,2,4);
+    }
+}
